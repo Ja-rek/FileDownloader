676a6323334a2efb248486fa7a8f999386d3a598
Ja-Rek <dto.jarek@gmail.com>
2019-04-13 22:19:44 +0200

    Add

---
+++ DownloadBot.Tests/Model.DownloadTest/LinkSeekerTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using DownloadBoot.Download;
+using System.Linq;
+
+namespace DownloadBot.Tests.Model.DownloadTest
+{
+    public class LinkSeekerTest 
+    {
+        [Test]
+        public void GetLinksToThreadPages_WhenPageContainLinks_ReturnsLinks()
+        {
+            var frontPageStub = WebsiteFake.FrontPage;
+
+            var expected = "http://www.AnyPage.com/unreal-for-mobile-and-standalone-vr/";
+            var actual = LinkSeeker.GetLinksToThreadPages(frontPageStub).FirstOrDefault();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetLinksToThreadPages_WhenPageNotContainLinks_ReturnsEmptyList()
+        {
+            var frontPageStub = string.Empty;
+
+            var expected = Enumerable.Empty<string>();
+            var actual = LinkSeeker.GetLinksToThreadPages(frontPageStub);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetLinksToFiles_WhenPageContainLinks_ReturnsLinks()
+        {
+            var threadPageStub = WebsiteFake.ThreadPage;
+
+            var expected = "http://file.AnyPage.com/20190414/Unreal for Mobile and Standalone VR.pdf";
+            var actual = LinkSeeker.GetLinksToFiles(threadPageStub).FirstOrDefault();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetLinksToFiles_WhenPageNotContainLinks_ReturnsEmptyList()
+        {
+            var frontPageStub = string.Empty;
+
+            var expected = Enumerable.Empty<string>();
+            var actual = LinkSeeker.GetLinksToFiles(frontPageStub);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ DownloadBot.Tests/Model.DownloadTest/WebsiteFake.cs
@@ -0,0 +1,9 @@
+﻿namespace DownloadBot.Tests.Model.DownloadTest
+{
+    public class WebsiteFake
+    {
+        public const string FrontPage = "<h2 class=\"entry-title\"><a href=\"http://www.AnyPage.com/unreal-for-mobile-and-standalone-vr/\" rel=\"bookmark\">Unreal for Mobile and Standalone VR</a></h2>"; 
+        
+        public const string ThreadPage = "<span class=\"download-links\"> <a href=\"http://file.AnyPage.com/20190414/Unreal for Mobile and Standalone VR.pdf\" target=\"_blank\" data-original-title=\"\" title=\"\"><i class=\"fa fa-download\" aria-hidden=\"true\"></i> Download PDF <span class=\"download-size\">(21 MB)</span></a> </span>";
+    }
+}
